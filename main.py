dabbbfe61efbf5e584f38baf1de4dfb77cf6bbef
jopulation <83256159+jopulation@users.noreply.github.com>
2024-09-09 23:36:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ gameBoard[4][5] = gameBoard[5][4] = '○'
 
 
 row = col = -1
-cnt = { 4, 2, 2}
+cnt = [ 4, 2, 2]
 # cnt[0] : 전체 돌의 개수
 # cnt[1] : 검은 돌의 개수
 # cnt[2] : 흰 돌의 개수
@@ -24,12 +24,12 @@ while not defs.gameOver(cnt):
         color = defs.colorChange(color)
         continue
     else: # 놓을 수 있는 경우
-        row, col = map(int, input("놓을 곳을 입력하세요. 예시: 1 8 ->", end = "").split())
+        row, col = map(int, input("놓을 곳을 입력하세요. 예시: 1 8 ->").split())
         # 숫자가 아닌 경우 에외처리(필요)
 
         while not defs.misPlace(gameBoard, color, row, col):
             row, col = map(int, input("놓을 수 없는 곳입니다.\n다시 입력해주세요. 예시: 1 8 ->", end = "").split())
     
-        defs.moving(gameBoard, color, row, col)
+        defs.moving(gameBoard, color, row, col, cnt)
 
 defs.resultPrint(cnt)
