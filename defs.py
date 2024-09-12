d695bd2a9d173d855412492e58d636557b8d5213
jopulation <83256159+jopulation@users.noreply.github.com>
2024-09-13 00:01:22 +0900

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,8 +1,12 @@
 import time
+import os
+
 offset =  [ [-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1] ]
 
 
 def boardPrint(gameBoard):
+    os.system('clear')
+    print("<Othello Game>")
     print("X 1 2 3 4 5 6 7 8")
     for i in range(1, 9):
         print(i, end="")
@@ -20,49 +24,18 @@ def gameOver(cnt):
 # cnt[1] : 검은 돌의 개수
 # cnt[2] : 흰 돌의 개수
 
-def colorchange(color):
+def colorChange(color):
     if color == '○':
         return '●'
     else:
         return '○'
-    
-def imposBoard(gameBoard, color):
-    oppcolor = colorchange(color)
-
-    for i in range(1, 9):
-        for j in range(1, 9):
-            if color == gameBoard[i][j]:
-
-                for k in range(0, 8):
-                    roww = i + offset[k][0]
-                    coll = j + offset[k][1]
-
-                    if oppcolor == gameBoard[roww][coll]:
-                        while not gameBoard[roww][coll] == ' ': # 가장자리이거나 빈자리이거나
-                            roww += offset[k][0]
-                            coll += offset[k][1]
-
-                            if gameBoard[roww][coll] == color: # 자신과 같은 색의 돌이면 안 됨
-                                break
-                        if roww == 0 or roww == 9 or coll == 0 or coll == 9:
-                            break #가장자리일 때
-                        return 0 # 놓을 수 있는 빈자리가 있을 때
-    return 1
-
-
-def areaCheck(row, col):
-    if (1 <= row <= 9) and (1 <= col <= 9):
-        return 1 # 영역 안에 있을 때
-    else:
-        return 0 # 영역 밖에 있을 때
-    
 
 def misPlace(gameBoard, color, row, col):
-    if (not areaCheck(row, col)) or (not gameBoard[row][col] == ' '):
-    # 영역 밖에 있거나 빈자리
+    if (not areaCheck(row, col)) or (not gameBoard[row][col] == '□'):
+    # 영역 밖에 있거나 빈자리가 아니거나
         return 1
     
-    oppColor = colorchange(color)
+    oppColor = colorChange(color)
 
     for i in range(0,8):
         roww = row + offset[i][0]
@@ -77,12 +50,32 @@ def misPlace(gameBoard, color, row, col):
         
     return 1 # 다 해보고도 안 되었을 때
 
+def imposBoard(gameBoard, color):
+    oppcolor = colorChange(color)
+
+    for i in range(1, 9):
+        for j in range(1, 9):
+            if gameBoard[i][j] == '□':
+                if not misPlace(gameBoard, color, i, j):
+                    return 0
+    return 1 # 다 해보고도 안 되었을 때
+
+
+def areaCheck(row, col):
+    if (1 <= row <= 9) and (1 <= col <= 9):
+        return 1 # 영역 안에 있을 때
+    else:
+        return 0 # 영역 밖에 있을 때
+    
+
+
+
 def moving(gameBoard, color, row, col, cnt):
     offCnt = [ 0, 0, 0, 0, 0, 0, 0, 0] # offCnt = [0 for i in range(8)]
     count = 0
     sum = 0
     roww = coll = -1
-    oppColor = colorchange(color)
+    oppColor = colorChange(color)
     gameBoard[row][col] = color
 
     for i in range(0, 8):
@@ -95,7 +88,7 @@ def moving(gameBoard, color, row, col, cnt):
             coll += offset[i][1]
             count += 1
                 
-        if gameBoard[roww][coll] == ' ': # 가장자리이거나 빈자리면 안 됨
+        if gameBoard[roww][coll] == '□': # 가장자리이거나 빈자리면 안 됨
             count = 0
         
         offCnt[i] = count
@@ -113,14 +106,14 @@ def moving(gameBoard, color, row, col, cnt):
 
     boardPrint(gameBoard) # 돌 놓은 위치부터 시작.
 
-    while sum > 0:
-        for i in range(1, 8): # 최대 7번까지 연속으로 뒤집을 수 있기 때문
+    for i in range(1, 8): # 최대 7번까지 연속으로 뒤집을 수 있기 때문
+        if sum > 0:
             for j in range(0, 8):
                 if offCnt[j] > 0:
                     gameBoard[row + i*offset[j][0]][col + i*offset[j][1]] = color
                     offCnt[j] -= 1
                     sum -= 1
-                    boardPrint(gameBoard)
+            boardPrint(gameBoard)
 
 def resultPrint(cnt):
     if cnt[1] > cnt[2]:
