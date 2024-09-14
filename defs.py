f78e463841918f8f979a408b7c1adcdce6d91c9b
jopulation <83256159+jopulation@users.noreply.github.com>
2024-09-14 23:49:11 +0900

    Update defs.py
    1. 빈자리 초록색으로 표시
    2. 놓을 수 있는 위치 '■'으로 표시

--- defs.py
+++ defs.py
@@ -3,22 +3,48 @@ import os
 
 offset =  [ [-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1] ]
 
-
 def boardPrint(gameBoard, N):
     os.system('clear')
     print("<Othello Game>")
+
     print("X", end="")
     for i in range(1, N+1):
         print("",i, end="")
     print()
+
     for i in range(1, N+1):
         print(i, end="")
         # 세로 열 출력
         for j in range(1, N+1):
-            print(" " + gameBoard[j][i], end="")
+            if gameBoard[j][i] == '□':
+                # 빈자리일 때는 초록으로 출력
+                print('\033[92m' + " " + gameBoard[j][i] + '\033[0m', end="")
+            else:
+                print(" " + gameBoard[j][i], end="")
         print()
     time.sleep(0.3)
 
+def ableBoardPrint(gameBoard, posBoard, N): # 놓을 수 있는 자리를 보여주는 기능 추가
+    os.system('clear')
+    print("<Othello Game>")
+
+    print("X", end="")
+    for i in range(1, N+1):
+        print("",i, end="")
+    print()
+
+    for i in range(1, N+1):
+        print(i, end="")
+        # 세로 열 출력
+        for j in range(1, N+1):
+            # 빈자리일 때는 초록으로 출력
+            if posBoard[j][i] == '■':
+                print('\033[92m' + " " + posBoard[j][i] + '\033[0m', end="")
+            elif gameBoard[j][i] == '□':
+                print('\033[92m' + " " + gameBoard[j][i] + '\033[0m', end="")
+            else:
+                print(" " + gameBoard[j][i], end="")
+        print()
 
 def gameOver(cnt, imp, N):
     if(cnt[0] == N*N or cnt[1] == 0 or cnt[2] == 0 or imp == 2):
@@ -28,13 +54,11 @@ def gameOver(cnt, imp, N):
 # cnt[1] : 검은 돌의 개수
 # cnt[2] : 흰 돌의 개수
 
-
 def colorChange(color):
     if color == '○':
         return '●'
     else:
         return '○'
-
 
 def areaCheck(row, col, N):
     if (1 <= row <= N+1) and (1 <= col <= N+1):
@@ -42,12 +66,11 @@ def areaCheck(row, col, N):
     else:
         return 0 # 영역 밖에 있을 때
     
-
 def misPlace(gameBoard, color, row, col, N):
     if (not areaCheck(row, col, N)) or (not gameBoard[row][col] == '□'):
     # 영역 밖에 있거나 빈자리가 아니거나
         return 1
-    
+
     oppColor = colorChange(color)
 
     for i in range(0, 8): # 8방향
@@ -59,19 +82,21 @@ def misPlace(gameBoard, color, row, col, N):
             coll += offset[i][1]
 
             if gameBoard[roww][coll] == color: # 상대돌이 끝나고 자신이 돌이 나올 때
-                return 0
-        
+                return 0 
     return 1 # 다 해보고도 안 되었을 때
 
-
-def imposBoard(gameBoard, color, N):
+def imposBoard(gameBoard, posBoard, color, N): # 모든 경우를 실행하고 가능한 자리를 표시.
+    able = 0
     for i in range(1, N+1):
         for j in range(1, N+1):
             if gameBoard[i][j] == '□':
                 if not misPlace(gameBoard, color, i, j, N):
-                    return 0
-    return 1 # 다 해보고도 안 되었을 때
-
+                    posBoard[i][j] = '■'
+                    able += 1
+    if able == 0:
+        return 1
+    else:
+        return 0
 
 def moving(gameBoard, color, row, col, cnt, N):
     offCnt = [0 for i in range(8)] # 8방향
@@ -90,14 +115,14 @@ def moving(gameBoard, color, row, col, cnt, N):
             roww += offset[i][0]
             coll += offset[i][1]
             count += 1
-                
+  
         if gameBoard[roww][coll] == '□': # 가장자리이거나 빈자리면 안 됨
             count = 0
-        
+
         offCnt[i] = count
         sum += count
         # 자신과 같은 색의 돌일 때
-    
+
     # 색에 따라서 cnt를 바꾸는 코드
     cnt[0] += 1
     if color == '●':
@@ -117,7 +142,7 @@ def moving(gameBoard, color, row, col, cnt, N):
                     offCnt[j] -= 1
                     sum -= 1
             boardPrint(gameBoard, N)
-
+    time.sleep(1)
 
 def resultPrint(cnt):
     if cnt[1] > cnt[2]:
@@ -126,4 +151,3 @@ def resultPrint(cnt):
         print("\n●:", cnt[1], "\n○:", cnt[2], "\nWINNNER : WHITE!")
     else:
         print("\n●:", cnt[1], "\n○:", cnt[2], "\nDRAW!")
-        
